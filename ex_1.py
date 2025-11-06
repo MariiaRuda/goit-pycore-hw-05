10936375d650f57fd56a7841d7653fe0624496ad
Mariia Ruda <masharuda290@gmail.com>
2025-11-06 18:41:57 +0200

    1_ex.comments

--- ex_1.py
+++ ex_1.py
@@ -1,11 +1,11 @@
 def caching_fibonacci():
-    cache={}
-    def fibonacci(n):
-         if n<=0 :
+    cache={} #Створити порожній словник cache
+    def fibonacci(n): #внутр. функція перевіряє n і записує в кеш
+         if n<=0 : #Якщо n <= 0, повернути 0
              return 0
-         if n==1 :
+         if n==1 :  #  Якщо n == 1, повернути 1
              return 1 
-         if n in cache:
+         if n in cache: #Якщо n у cache, повернути cache[n]
              return cache[n]  
          
          cache[n] = fibonacci(n-1)+ fibonacci(n-2)
@@ -13,6 +13,6 @@ def caching_fibonacci():
     
     return fibonacci
 
-fib=caching_fibonacci()
+fib=caching_fibonacci() #присвоюємо функцію
 print(fib(10))
 print(fib(20))
